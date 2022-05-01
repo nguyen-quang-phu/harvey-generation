b402c60d4b26e93f90cc6794d8afb9a84a768f79
Nguyễn Quang Phú <nqphu1998@gmail.com>
2022-05-01 23:17:00 +0700

    add feature generate snippet

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
 import HelloWorld from "./HelloWorld";
 import GenerateEditorConfig from "./GenerateEditorConfig";
+import GenerateSnippet from "./GenerateSnippet";
 
-const listCommands = [HelloWorld, GenerateEditorConfig]
+const listCommands = [HelloWorld, GenerateEditorConfig, GenerateSnippet];
 
 export default listCommands;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_EDITOR_CONFIG_FILE = "DefaultTemplate.editorconfig";
 export const TEMPLATE_FOLDER = "templates";
 export const EDITOR_CONFIG_FILE = ".editorconfig";
+export const VSCODE_FOLDER = ".vscode";
